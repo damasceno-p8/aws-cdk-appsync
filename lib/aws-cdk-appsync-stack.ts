2b6f9a8a63ed4512b4a9b36f8e7a5fb0f7690b3d
Bruno Damasceno <bruno.damasceno@produce8.com>
2022-05-13 10:29:37 -0300

    refactor: define setup to new cdk version

--- lib/aws-cdk-appsync-stack.ts
+++ lib/aws-cdk-appsync-stack.ts
@@ -1,8 +1,37 @@
-import { Stack, StackProps } from "aws-cdk-lib"
-import { Construct } from "constructs"
+import * as appSync from '@aws-cdk/aws-appsync';
+import * as cdk from '@aws-cdk/core';
 
-export class AwsCdkAppsyncStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class AwsCdkAppsyncStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    const api = new appSync.GraphqlApi(this, "Api", {
+      name: "cdk-notes-appsync-api",
+      schema: appSync.Schema.fromAsset("graphql/schema.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appSync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365)),
+          },
+        },
+      },
+      xrayEnabled: true,
+    })
+
+    // Prints out the AppSync GraphQL endpoint to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIURL", {
+      value: api.graphqlUrl,
+    })
+
+    // Prints out the AppSync GraphQL API key to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || "",
+    })
+
+    // Prints out the stack region to the terminal
+    new cdk.CfnOutput(this, "Stack Region", {
+      value: this.region,
+    })
   }
 }
